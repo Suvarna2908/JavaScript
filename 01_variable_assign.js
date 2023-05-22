1c6ffbf8df54d34ee71c9d74b95a3aecc311f3e0
Suvarna2908 <suvarnashivarkar@gmail.com>
2023-05-22 16:06:45 +0530

    Assigment done
    Assigment done

---
+++ 01_variable_assign.js
@@ -0,0 +1,40 @@
+
+
+
+console.log("********** Question 1 *********");
+
+var myFullName;
+
+console.log(myFullName);
+
+myFullName = "Suvarna Jagtap";
+
+console.log(myFullName);
+
+myFullName = "suvarna vikram jagtap";
+
+console.log(myFullName);
+
+console.log("********** Question 2 *********");
+
+var myLove = "I Love only JS";
+
+console.log(myLove);
+
+console.log("********** Question 3 *********");
+
+var firstName ="Suvarna";
+var lastName = "Jagtap";
+var age="29";
+var address="Saswad";
+var pinCode="4123031";
+var gender = "female";
+var isMarried = "yes";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pinCode);
+console.log(gender);
+console.log(isMarried);
